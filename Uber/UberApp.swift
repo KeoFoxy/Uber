37f769fddb75235360941125e516f4a97ec0ddc5
KeoFoxy <arasdiel68@gmail.com>
2023-07-17 23:40:00 +0300

    Initial Commit

---
+++ Uber/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Alexander Sorokin on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
