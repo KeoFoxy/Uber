49d4e246350bee21cb7d3a3a8d05756f0dfe3d79
KeoFoxy <arasdiel68@gmail.com>
2023-07-18 17:19:46 +0300

    Cell for a scrollview

---
+++ Uber/Core/LocationSearch/Views/LocationSearchResultCell.swift
@@ -0,0 +1,39 @@
+//
+//  LocationSearchResultCell.swift
+//  Uber
+//
+//  Created by Alexander Sorokin on 18.07.2023.
+//
+
+import SwiftUI
+
+struct LocationSearchResultCell: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "mappin.circle.fill")
+                .resizable()
+                .foregroundColor(.blue)
+                .accentColor(.white)
+                .frame(width: 40, height: 40)
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Starbucks Coffee")
+                    .font(.body)
+                
+                Text("123 Main St, Cupertino CA")
+                    .font(.system(size: 15))
+                    .foregroundColor(.gray)
+                
+                Divider()
+            }
+            .padding(.leading, 8)
+            .padding(.vertical, 8)
+        }
+        .padding(.leading)
+    }
+}
+
+struct LocationSearchResultCell_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationSearchResultCell()
+    }
+}
