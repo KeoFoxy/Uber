49743d571206fae8703bf2cc9b3bd0dee64cc3a3
KeoFoxy <arasdiel68@gmail.com>
2023-07-18 18:14:03 +0300

    searchCompletion

---
+++ Core/LocationSearch/ViewModels/LocationSearchViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  LocationSearchViewModel.swift
+//  Uber
+//
+//  Created by Alexander Sorokin on 18.07.2023.
+//
+
+import Foundation
+import MapKit
+
+class LocationSearchViewModel: NSObject, ObservableObject {
+    // MARK: Properties
+    
+    @Published var results = [MKLocalSearchCompletion]()
+    private let searchCompleter = MKLocalSearchCompleter()
+    var queryFragment: String = "" {
+        didSet {
+            searchCompleter.queryFragment = queryFragment
+        }
+    }
+    
+    override init() {
+        super.init()
+        searchCompleter.delegate = self
+        searchCompleter.queryFragment = queryFragment
+    }
+}
+
+// MARK: - MKLocalSearchCompleterDelegate
+
+extension LocationSearchViewModel: MKLocalSearchCompleterDelegate {
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        self.results = completer.results
+    }
+}
