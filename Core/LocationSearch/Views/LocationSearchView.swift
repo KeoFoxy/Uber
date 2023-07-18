a405b4cf2bea864ca9f8ed991dd3ec064cd61a46
KeoFoxy <arasdiel68@gmail.com>
2023-07-18 18:13:26 +0300

    Replaced hardcoded values

--- Core/LocationSearch/Views/LocationSearchView.swift
+++ Core/LocationSearch/Views/LocationSearchView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct LocationSearchView: View {
     @State private var startLocationText = ""
     @State private var destinationLocationText = ""
-    
+    @StateObject var viewModel = LocationSearchViewModel()
     
     var body: some View {
         VStack {
@@ -37,7 +37,7 @@ struct LocationSearchView: View {
                             .systemGroupedBackground))
                         .padding(.trailing)
                     
-                    TextField("Where to?", text: $destinationLocationText)
+                    TextField("Where to?", text: $viewModel.queryFragment)
                         .frame(height: 32)
                         .background(Color(
                             .systemGray4))
@@ -54,8 +54,10 @@ struct LocationSearchView: View {
             
             ScrollView {
                 VStack(alignment: .leading) {
-                    ForEach(0..<20, id: \.self) { _ in
-                        LocationSearchResultCell()
+                    ForEach(viewModel.results, id: \.self) { result in
+                        LocationSearchResultCell(
+                            title: result.title,
+                            subtitle: result.subtitle)
                     }
                 }
             }
