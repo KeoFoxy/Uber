ad992323784987a1171b2ae6fa072061e3d642e7
KeoFoxy <arasdiel68@gmail.com>
2023-07-18 18:13:51 +0300

    Title and subtitle parameter in Cell

--- Core/LocationSearch/Views/LocationSearchResultCell.swift
+++ Core/LocationSearch/Views/LocationSearchResultCell.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct LocationSearchResultCell: View {
+    let title: String
+    let subtitle: String
+    
     var body: some View {
         HStack {
             Image(systemName: "mappin.circle.fill")
@@ -16,10 +19,10 @@ struct LocationSearchResultCell: View {
                 .accentColor(.white)
                 .frame(width: 40, height: 40)
             VStack(alignment: .leading, spacing: 4) {
-                Text("Starbucks Coffee")
+                Text(title)
                     .font(.body)
                 
-                Text("123 Main St, Cupertino CA")
+                Text(subtitle)
                     .font(.system(size: 15))
                     .foregroundColor(.gray)
                 
@@ -34,6 +37,6 @@ struct LocationSearchResultCell: View {
 
 struct LocationSearchResultCell_Previews: PreviewProvider {
     static var previews: some View {
-        LocationSearchResultCell()
+        LocationSearchResultCell(title: "Coffee", subtitle: "123, Cupertino CA")
     }
 }
