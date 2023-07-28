a499e24d86cbfdbbd5cddcf461795991c9bf283f
KeoFoxy <arasdiel68@gmail.com>
2023-07-28 22:45:18 +0300

    Registration View

---
+++ Core/Authentication/Views/RegistrationView.swift
@@ -0,0 +1,77 @@
+//
+//  RegistrationView.swift
+//  Uber
+//
+//  Created by Alexander Sorokin on 28.07.2023.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    @State private var fullname: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    
+    var body: some View {
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "arrow.left")
+                        .font(.title)
+                        .imageScale(.medium)
+                        .padding()
+                    
+                }
+                
+                Text("Create new account")
+                    .font(.system(size: 40))
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.leading)
+                    .frame(width: 250)
+                
+                Spacer()
+                
+                VStack {
+                    VStack(spacing: 56) {
+                        CustomInputField(text: $fullname, title: "Full Name", placeholder: "Enter your name")
+                        
+                        CustomInputField(text: $email, title: "Email Address", placeholder: "name@example.com")
+                        
+                        CustomInputField(text: $password, title: "Create Password", placeholder: "Enter your password", isSecureField: true)
+                    }
+                    .padding(.leading)
+                    
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Text("Sign Up")
+                                .fontWeight(.bold)
+                                .foregroundColor(.black)
+                        }
+                        .frame(width: UIScreen.main.bounds.width - 32, height: 50)
+                    }
+                    .background(.white)
+                    .cornerRadius(10)
+                    
+                    Spacer()
+                }
+            }
+            .foregroundColor(.white)
+        }
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
