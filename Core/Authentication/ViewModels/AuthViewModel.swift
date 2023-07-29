69a3c1bf7142afe07e4a8b01f0c33ca5f59ed4aa
KeoFoxy <arasdiel68@gmail.com>
2023-07-29 23:02:39 +0300

    Sign In, out and store + fetch user data

--- Core/Authentication/ViewModels/AuthViewModel.swift
+++ Core/Authentication/ViewModels/AuthViewModel.swift
@@ -7,12 +7,25 @@
 
 import Foundation
 import Firebase
+import FirebaseFirestoreSwift
 
 class AuthViewModel: ObservableObject {
     @Published var userSession: FirebaseAuth.User?
     
     init() {
         userSession = Auth.auth().currentUser
+        fetchUser()
+    }
+    
+    func signIn(withEmail email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("DEBUG: Failed to sign in with error \(error.localizedDescription)")
+                return
+            }
+            
+            self.userSession = result?.user
+        }
     }
     
     func registerUser(withEmai email: String, password: String, fullname: String) {
@@ -21,9 +34,31 @@ class AuthViewModel: ObservableObject {
                 print("DEBUG: Failed to sign up with error \(error.localizedDescription)")
                 return
             }
+            guard let firebaseUser = result?.user else { return }
+            self.userSession = firebaseUser
             
-            print("DEBUG: Registred user successfully")
-            print("DEBUG: User is: \(result?.user.uid)")
+            let user = User(fullname: fullname, email: email, uid: firebaseUser.uid)
+            guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
+            
+            Firestore.firestore().collection("users").document(firebaseUser.uid).setData(encodedUser)
+        }
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+            self.userSession = nil
+        } catch let error {
+            print("DEBUG: Failed to sign out with error: \(error.localizedDescription)")
+        }
+    }
+    
+    func fetchUser() {
+        guard let uid = self.userSession?.uid else { return }
+        Firestore.firestore().collection("users").document(uid).getDocument { snapshot, _ in
+            guard let snapshot = snapshot else { return }
+            
+            guard let user = try? snapshot.data(as: User.self) else { return }
         }
     }
 }
