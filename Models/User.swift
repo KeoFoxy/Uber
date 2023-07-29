e8d2987725d08b4cfe618c99880ad3ec2caf6d13
KeoFoxy <arasdiel68@gmail.com>
2023-07-29 23:01:22 +0300

    User Data Firebase Model

---
+++ Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Uber
+//
+//  Created by Alexander Sorokin on 29.07.2023.
+//
+
+import Foundation
+
+struct User: Codable {
+    let fullname: String
+    let email: String
+    let uid: String
+}
