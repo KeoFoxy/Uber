325d834b8d14283a8a2648b157641182013655b0
KeoFoxy <arasdiel68@gmail.com>
2023-07-30 23:16:05 +0300

    Extraction of the Location Search Results

--- ReusableItems/LocationSearchResultsView.swift
+++ ReusableItems/LocationSearchResultsView.swift
@@ -5,4 +5,26 @@
 //  Created by Alexander Sorokin on 30.07.2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct LocationSearchResultView: View {
+    @StateObject var viewModel: LocationSearchViewModel
+    let config: LocationResultsViewConfig
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                ForEach(viewModel.results, id: \.self) { result in
+                    LocationSearchResultCell(
+                        title: result.title,
+                        subtitle: result.subtitle)
+                    .onTapGesture {
+                        withAnimation(.spring()) {
+                            viewModel.selectLocation(result, config: config)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
