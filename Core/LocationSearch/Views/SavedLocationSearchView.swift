8ba1741c8c9b2ad1fe851a75ef028b2172e2afaa
KeoFoxy <arasdiel68@gmail.com>
2023-07-31 01:01:47 +0300

    Remove left arrow

--- Core/LocationSearch/Views/SavedLocationSearchView.swift
+++ Core/LocationSearch/Views/SavedLocationSearchView.swift
@@ -14,19 +14,11 @@ struct SavedLocationSearchView: View {
     
     var body: some View {
         VStack {
-            HStack(spacing: 16) {
-                Image(systemName: "arrow.left")
-                    .font(.title2)
-                    .imageScale(.medium)
-                    .padding(.leading)
-                
-                TextField("Search for a location...", text: $viewModel.queryFragment)
-                    .frame(height: 32)
-                    .padding(.leading)
-                    .background(Color(.systemGray5))
-                    .padding(.trailing)
-            }
-            .padding(.top)
+            TextField("Search for a location...", text: $viewModel.queryFragment)
+                .frame(height: 32)
+                .padding(.leading)
+                .background(Color(.systemGray5))
+                .padding()
             
             Spacer()
             
@@ -34,6 +26,7 @@ struct SavedLocationSearchView: View {
             
         }
         .navigationTitle(config.subtitle)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
