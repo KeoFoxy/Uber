3d87afab3b75f21a280eea1bf4f6b1f7acfb7bd3
KeoFoxy <arasdiel68@gmail.com>
2023-07-30 19:19:06 +0300

    Settings View

---
+++ Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,49 @@
+//
+//  SettingsView.swift
+//  Uber
+//
+//  Created by Alexander Sorokin on 30.07.2023.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        VStack {
+            List {
+                Section {
+                    // User Info Header
+                    HStack {
+                        UserInfoHeader(avatarImageName: "avatar-preview",
+                                       userFullname: "user.fullname",
+                                       userEmail: "user.email",
+                                       isChevronDisplayed: true)
+                    }
+                }
+                
+                Section("Favourites") {
+                    CategoryCellView(title: "Home", subtitle: "Add Home", imageName: "house.circle.fill", fontWeight: .semibold, isChevronDisplayed: true)
+                    
+                    CategoryCellView(title: "Work", subtitle: "Add Work", imageName: "archivebox.circle.fill", fontWeight: .semibold, isChevronDisplayed: true)
+                }
+                
+                Section("Settings") {
+                    CategoryCellView(title: "Notifications", imageName: "bell.circle.fill", imageColor: Color(.systemPurple))
+                    
+                    CategoryCellView(title: "Payment Methods", imageName: "creditcard.circle.fill", imageColor: Color(.systemOrange))
+                }
+                
+                Section("Account") {
+                    CategoryCellView(title: "Make Money Driving", imageName: "dollarsign.circle.fill", imageColor: Color(.systemGreen))
+                    CategoryCellView(title: "Sign Out", imageName: "arrow.left.circle.fill", imageColor: Color(.systemRed))
+                }
+            }
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
